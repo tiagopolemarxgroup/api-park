74d5b7d6c04afcb0e42ea593d5da34d62b18beaf
Tiago <tiago.polemarxgroup@gmail.com>
2023-12-03 15:36:16 -0300

    Operações de crud

--- src/main/java/com/tec/api/park/controller/UsuarioController.java
+++ src/main/java/com/tec/api/park/controller/UsuarioController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -15,8 +17,30 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getByid(@PathVariable Long id) {
+        Usuario user = usuarioService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll(){
+        return ResponseEntity.ok(usuarioService.findAll());
+    }
+
+//    @PatchMapping("/{id}")
+//    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id,@RequestBody Usuario usuario){
+//        Usuario user = usuarioService.updateSenha(id, usuario);
+//        return ResponseEntity.ok(user);
+//    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarSenha(@PathVariable Long id,@RequestBody Usuario usuario){
+        Usuario user = usuarioService.updateSenhaV2(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/tec/api/park/service/UsuarioService.java
+++ src/main/java/com/tec/api/park/service/UsuarioService.java
@@ -6,13 +6,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-   @Transactional
+    @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
+
+    @Transactional(readOnly = true)
+    public Usuario findById(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    @Transactional
+    public Usuario updateSenha(Long id, Usuario usuario) {
+        Usuario byId = this.findById(id);
+        byId.setPassword(usuario.getPassword());
+        return usuarioRepository.save(byId);
+    }
+
+    @Transactional
+    public Usuario updateSenhaV2(Long id, String password) {
+        Usuario usuario = findById(id);
+        usuario.setPassword(password);
+        return usuario;
+    }
+
 }
